dc27a264d7cf061e948a1a38a902d4fd8d4aea19
ts moosavi <tt.musavi@gmail.com>
2023-03-24 04:28:44 +0430

    complete second jetpack compose practice

--- app/src/main/java/com/example/composebasicspractice/MainActivity.kt
+++ app/src/main/java/com/example/composebasicspractice/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +32,8 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
 //                    Greeting("Android")
-                    MainScreen()
+//                    FirstPracticeScreen()
+                    SecondPracticeScreen()
                 }
             }
         }
@@ -39,13 +41,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier){
+fun FirstPracticeScreen(modifier: Modifier = Modifier){
 val picture = painterResource(R.drawable.compose_background)
     Column {
         Image(painter = picture , contentDescription = null)
         Text(text = stringResource(R.string.jetpack_compose_tutorial), fontSize = 24.sp, modifier = Modifier.padding(10.dp))
         Text(text = stringResource(R.string.jetpack_compose_explanation), modifier = Modifier.padding(start = 16.dp, end = 16.dp), textAlign = TextAlign.Justify)
         Text(text = stringResource(R.string.tutorial_explanation), modifier = Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+
+    }
+}
+
+@Composable
+fun SecondPracticeScreen(modifier: Modifier = Modifier){
+    val picture = painterResource(R.drawable.task_completed)
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = picture , contentDescription = null)
+        Text(text = stringResource(R.string.task_completed), fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 24.dp, bottom = 8.dp))
+        Text(text = stringResource(R.string.nice_work), fontSize = 16.sp)
 
     }
 }
@@ -60,6 +73,7 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeBasicsPracticeTheme {
 //        Greeting("Android")
-        MainScreen()
+//        FirstPracticeScreen()
+        SecondPracticeScreen()
     }
 }
